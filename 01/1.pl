33a5f2aad8c2d00c3c59b3e3c8fa2762dc7be34d
Piotr Szulc <szulo123@gmail.com>
2015-12-02 20:18:45 +0100

    Puzzle 01b

--- 01/1.pl
+++ 01/1.pl
@@ -5,6 +5,8 @@ use warnings;
 use v5.10;
 
 foreach my $l (<>) {
+	my $basement = 0;
+	my $pos = 1;
 	my $f = 0;
 	foreach my $s (split '', $l) {
 		if ($s eq '(') {
@@ -12,6 +14,12 @@ foreach my $l (<>) {
 		} elsif ($s eq ')') {
 			$f--;
 		}
+		if (!$basement && $f < 0) {
+			say "Basement at " . $pos;
+			$basement = 1;
+		}
+
+		$pos++
 	}
-	say $f;
+	say "Ended up at " . $f;
 }
