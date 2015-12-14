1eee725112cd767694dc2b54c558a298d54c0525
Piotr Szulc <szulo123@gmail.com>
2015-12-14 22:39:39 +0100

    Puzzle 12a

---
+++ 12/Puzzle12/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Puzzle12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var filename in args)
+            {
+                using (var reader = new JsonTextReader(new StreamReader(filename)))
+                {
+                    long sum = 0;
+                    while (reader.Read())
+                    {
+                        if (reader.TokenType == JsonToken.Integer)
+                        {
+                            sum += (long) reader.Value;
+                        }
+                    }
+
+                    Console.WriteLine("Sum: {0}", sum);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
