997004556f2bac9b99fca45cf09998253f585b31
Piotr Szulc <szulo123@gmail.com>
2016-12-16 01:08:19 +0100

    Solved 2016/11

---
+++ 2016/Puzzle11/Puzzle11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Puzzle11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(args[0]);
+            var solver = new Solver(input);
+
+            var task1 = new Task<int>(() => solver.Solve1());
+            var task2 = new Task<int>(() => solver.Solve2());
+            task1.Start();
+            task2.Start();
+
+            Console.WriteLine(task1.Result);
+            Console.WriteLine(task2.Result);
+
+            Console.ReadLine();
+        }
+    }
+}
