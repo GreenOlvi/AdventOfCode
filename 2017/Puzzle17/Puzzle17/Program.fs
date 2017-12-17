bef42d5cbdaace6561aef040d9965bce38edd9d0
Piotr Szulc <szulo123@gmail.com>
2017-12-17 23:35:00 +0100

    Solved 2017/17 part 1

---
+++ 2017/Puzzle17/Puzzle17/Program.fs
@@ -0,0 +1,60 @@
+﻿open System
+open Utils.Common
+
+let insertAfter index element (list:'a list) =
+    let before = Seq.take (index + 1) list |> List.ofSeq
+    let after = list |> Seq.skip (index + 1) |> List.ofSeq
+    Seq.concat [before; [element]; after] |> List.ofSeq
+
+let stepOn steps elements start =
+    (start + steps) % elements
+
+let solve1 input =
+    let rec runSteps (buffer:int list) pos n limit =
+        match n with
+        | a when a = limit -> buffer
+        | _ ->
+            let i = stepOn input buffer.Length pos
+            let newBuffer = insertAfter i n buffer
+            runSteps newBuffer (i + 1) (n + 1) limit
+    let buffer = runSteps [0] 0 1 2018
+    buffer.[buffer |> List.findIndex (fun i -> i = 2017) |> (+) 1]
+
+let solve2 input =
+    let limit = 50000001
+    let rec loop elems atOne pos = 
+        match elems with
+        | a when a = limit -> atOne
+        | _ ->
+            let i = stepOn input elems pos
+            let el = if i = 0 then elems else atOne
+            loop (elems + 1) el i
+    loop 1 0 0
+
+[<EntryPoint>]
+let main _ = 
+    let input = 344
+
+    printfn "--- Tests ---\n"
+    test3 insertAfter [
+        ((2, 4, [0; 1; 2; 3]), [0; 1; 2; 4; 3]);
+        ((3, 4, [0; 1; 2; 3]), [0; 1; 2; 3; 4]);
+        ((0, 4, [0; 1; 2; 3]), [0; 4; 1; 2; 3]);
+        ((0, 1, [0]), [0; 1]);
+    ];
+
+    test3 stepOn [
+        ((10, 15, 2), 12);
+        ((10, 15, 8), 3);
+    ];
+
+    test solve1 [(3, 638)]
+
+    let result1 = timeIt (fun () -> solve1 input)
+    printfn "Result 1: %A" result1
+
+    let result2 = timeIt (fun () -> solve2 input)
+    printfn "Result 2: %A" result2
+
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
--- 2017/Utils/Utils/Common.fs
+++ 2017/Utils/Utils/Common.fs
@@ -3,18 +3,17 @@
 open System
 
 module Common =
-
     let wrapColor color printAction =
         let old = System.Console.ForegroundColor
         System.Console.ForegroundColor <- color
         printAction()
         System.Console.ForegroundColor <- old
 
-    let test f data =
+    let testN findResultFun data =
         let testResults =
             data
             |> List.map (fun (input, expected) -> 
-                let result = input |> f
+                let result = findResultFun input
                 printf "input: %A, result: %A, " input result
                 if (result = expected) then
                     wrapColor ConsoleColor.Green (fun () -> printfn "OK")
@@ -22,9 +21,12 @@ module Common =
                     wrapColor ConsoleColor.Red (fun () -> printfn "expected: %A" expected)
                 result = expected
             )
-        let successful = (testResults |> List.filter (fun i -> i) |> List.length)
+        let successful = (testResults |> List.filter id |> List.length)
         let color = match successful = data.Length with | true -> ConsoleColor.Green | false -> ConsoleColor.Red
         wrapColor color (fun () -> printfn "Success %A/%A\n" successful data.Length)
+
+    let test f data = testN (fun a -> f a) data
+    let test3 f data = testN (fun (a, b, c) -> f a b c) data
 
     let splitString (chars:char[]) (input:string) = input.Split(chars, StringSplitOptions.RemoveEmptyEntries)
 
