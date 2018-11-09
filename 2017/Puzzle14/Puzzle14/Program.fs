f548fd34a75c3ea56fbf9562a19d9ab14abe0433
Piotr Szulc <szulo123@gmail.com>
2018-11-09 21:13:53 +0100

    Solved 2017/14

---
+++ 2017/Puzzle14/Puzzle14/Program.fs
@@ -0,0 +1,117 @@
+﻿open System
+open Utils
+
+let hexToBit c =
+    match c with
+    | '0' -> "0000"
+    | '1' -> "0001"
+    | '2' -> "0010"
+    | '3' -> "0011"
+    | '4' -> "0100"
+    | '5' -> "0101"
+    | '6' -> "0110"
+    | '7' -> "0111"
+    | '8' -> "1000"
+    | '9' -> "1001"
+    | 'a' -> "1010"
+    | 'b' -> "1011"
+    | 'c' -> "1100"
+    | 'd' -> "1101"
+    | 'e' -> "1110"
+    | 'f' -> "1111"
+    | _ -> invalidArg "c" "Character should be one of [0-9a-f]"
+
+let hexToBinary input =
+    input
+    |> Seq.map hexToBit
+    |> String.concat String.Empty
+
+let getRowSequence input =
+    seq {0..127}
+    |> Seq.map (fun i -> input + "-" + string i)
+
+let countOnes (input:string) =
+    input
+    |> Seq.map (fun c ->
+        match c with
+        | '0' -> 0
+        | '1' -> 1
+        | _ -> invalidArg "c" "Should be 0 or 1")
+    |> Seq.sum
+
+let binaryToSet (hashList:string seq) =
+    hashList
+    |> Seq.mapi (fun y s ->
+        s
+        |> Seq.mapi (fun x b -> (x, b))
+        |> Seq.filter (fun (_, b) -> match b with | '1' -> true | _ -> false)
+        |> Seq.map (fun (x, _) -> (x, y))
+    )
+    |> Seq.concat
+    |> Set.ofSeq
+
+let findRegions (input:(int*int) Set) : int =
+    let rec round (count:int) (toCheck:(int*int) list) (positions:(int*int) Set) =
+        match toCheck with
+        | h::t ->
+            let (x, y) = h
+            let neighbours =
+                [(x - 1, y); (x + 1, y); (x, y - 1); (x, y + 1)]
+                |> List.filter (fun p -> Set.contains p positions)
+            round count (List.concat [t; neighbours]) (positions.Remove h)
+        | [] ->
+            match Set.isEmpty positions with
+            | true -> count
+            | false ->
+                let h = Set.minElement positions
+                round (count + 1) [h] (Set.remove h positions)
+    round 0 [] input
+
+let solve1 input =
+    getRowSequence input
+    |> Seq.map (KnotHash.knotHash >> hexToBinary >> countOnes)
+    |> Seq.sum
+
+let solve2 input =
+    let map =
+        getRowSequence input
+        |> Seq.map (KnotHash.knotHash >> hexToBinary)
+        |> binaryToSet
+        |> findRegions
+    map
+
+[<EntryPoint>]
+let main _ = 
+    let input = "oundnydw"
+
+    Common.test hexToBinary [
+        ("a0c2017", "1010000011000010000000010111")
+    ]
+
+    Common.test solve1 [
+        ("flqrgnkx", 8108);
+    ]
+
+    let result1 = Common.timeIt (fun () -> solve1 input)
+    printfn "Result 1: %A" result1
+
+    Common.test binaryToSet [
+        (["0110"; "1010"], set [(1, 0); (2, 0); (0, 1); (2, 1)])
+    ]
+
+    Common.test findRegions [
+        (set [], 0);
+        (set [(1,1)], 1);
+        (set [(1, 0); (2, 0); (0, 1); (2, 1)], 2);
+        (set [(0, 0); (2, 0); (1, 1); (3, 1)], 4);
+    ]
+
+    Common.test solve2 [
+        ("flqrgnkx", 1242)
+    ]
+
+    let result2 = Common.timeIt (fun () -> solve2 input)
+    printfn "Result 2: %A" result2
+
+    Console.ReadLine() |> ignore
+    0 // return an integer exit code
---
+++ 2017/Utils/Utils/KnotHash.fs
@@ -0,0 +1,67 @@
+﻿namespace Utils
+
+open System
+
+module KnotHash =
+
+    type State = { position:int; skipSize:int; list:int list }
+
+    let reverseFragment start length l =
+        let L = Seq.length l
+        if length > L then
+            invalidArg "length" "Fragment length should be shorter than list"
+        let boundStart = start % L
+        let double = Seq.concat [|l; l|]
+        let preFragment = double |> Seq.take boundStart
+        let postFragment = double |> Seq.skip (boundStart + length)
+        let fragment = double |> Seq.skip boundStart |> Seq.take length |> Seq.rev
+        let newDouble = Seq.concat [|preFragment; fragment; postFragment|]
+        let overflow =
+            match (start + length > L) with
+            | true -> start + length - L
+            | false -> 0
+        let newStart =
+            match overflow with
+            | 0 -> Seq.empty
+            | o -> newDouble |> Seq.skip L |> Seq.take o
+        let newEnd = newDouble |> Seq.skip overflow |> Seq.take (L - overflow)
+        Seq.concat [newStart; newEnd] |> List.ofSeq
+
+    let step (s:State) l :State =
+        let newList = reverseFragment s.position l s.list
+        {
+            position = (s.position + l + s.skipSize) % (List.length s.list);
+            skipSize = s.skipSize + 1;
+            list = newList;
+        }
+
+    let round initialState input = input |> List.fold step initialState
+
+    let rounds count initialState input =
+        let rec r state i =
+            match i with
+            | 0 -> state
+            | _ ->
+                let s = round state input
+                r s (i - 1)
+        r initialState count
+
+    let toAscii (string:String) =
+        System.Text.ASCIIEncoding.ASCII.GetBytes(string)
+        |> List.ofArray
+        |> List.map Convert.ToInt32
+
+    let xor values = List.fold (^^^) 0 values
+
+    let bytesToHex (bytes:int list) =
+        bytes
+        |> List.map (fun (b:int) -> System.String.Format("{0:x2}", b))
+        |> String.concat String.Empty
+
+    let initialState = { position = 0; skipSize = 0; list = List.ofSeq {0..255}}
+
+    let knotHash input =
+        let newInput = List.concat [input |> toAscii; [17; 31; 73; 47; 23]]
+        let state = rounds 64 initialState newInput
+        let chunks = state.list |> List.chunkBySize 16
+        chunks |> List.map xor |> bytesToHex
