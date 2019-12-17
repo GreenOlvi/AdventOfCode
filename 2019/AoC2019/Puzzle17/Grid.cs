3a3bc25ecc0106715f6f117cc51d73f6b3d81d2e
Piotr Szulc <szulo123@gmail.com>
2019-12-17 07:50:06 +0100

    wip

--- 2019/AoC2019/Puzzle17/Grid.cs
+++ 2019/AoC2019/Puzzle17/Grid.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using AoC2019.Common;
 
@@ -10,23 +11,38 @@ namespace AoC2019.Puzzle17
         {
             var o = display.ToList();
 
-            var width = o.IndexOf('\n');
-            var height = o.Count() / width;
-
+            var width = o.IndexOf('\n') + 1;
             var scaffolding = o.Select((c, i) => (c, i))
                 .Where(p => p.c == '#' || p.c == '^')
-                .Select(p => new Position(p.i % (width + 1), p.i / (width + 1)));
+                .Select(p => new Position(p.i % width, p.i / width));
 
             _set = scaffolding.ToHashSet();
             _width = _set.Max(p => p.X) + 1;
             _height = _set.Max(p => p.Y) + 1;
             _output = new string(o.ToArray());
+
+            var robot = o.Select((c, i) => (c, i))
+                .First(p => p.c == '^' || p.c == 'v' || p.c == '>' || p.c == '<');
+            RobotPosition = new Position(robot.i % width, robot.i / width);
+            RobotDirection = robot.c switch
+            {
+                '^' => Direction.Up,
+                'v' => Direction.Down,
+                '>' => Direction.Right,
+                '<' => Direction.Down,
+                _ => throw new ArgumentOutOfRangeException(nameof(robot.c)),
+            };
         }
 
         private readonly HashSet<Position> _set;
         private readonly int _width;
         private readonly int _height;
         private readonly string _output;
+
+        public Position RobotPosition { get;  }
+        public Direction RobotDirection { get;  }
+
+        public bool IsScaffold(Position position) => _set.Contains(position) ? true : false;
 
         public IEnumerable<Position> GetIntersections() =>
             _set.Where(p => p.X > 0 && p.Y > 0 && p.X < _width && p.Y < _height)
--- 2019/AoC2019/Puzzle17/Solution.cs
+++ 2019/AoC2019/Puzzle17/Solution.cs
@@ -24,10 +24,85 @@ namespace AoC2019.Puzzle17
             return grid.GetIntersections().Select(p => p.X * p.Y).Sum();
         }
 
+        public static long Solve2(IEnumerable<long> input)
+        {
+            var mod = new[] { 2L }.Concat(input.Skip(1));
+            var r = new Robot(new IntcodeMachine(mod));
+
+            var path = string.Join(",", r.FindPath());
+            Console.WriteLine(path);
+
+            return 0;
+        }
+
         public Task<string> Solve1Async() =>
             Task.Run(() => Solve1(_input).ToString());
 
         public Task<string> Solve2Async() =>
-            throw new NotImplementedException();
+            Task.Run(() => Solve2(_input).ToString());
+    }
+
+    public class Robot
+    {
+        public Robot(IntcodeMachine machine)
+        {
+            _machine = machine;
+            _machine.Run();
+
+            var o = _machine.GetAllOutput().Select(i => (char)i);
+            _grid = new Grid(o);
+
+            _position = _grid.RobotPosition;
+            _direction = _grid.RobotDirection;
+        }
+
+        private readonly IntcodeMachine _machine;
+        private Grid _grid;
+
+        private Position _position;
+        private Direction _direction;
+
+        public IEnumerable<string> FindPath()
+        {
+            var i = 0;
+            var finished = false;
+            while (!finished)
+            {
+                var front = _position.Move(_direction);
+                if (_grid.IsScaffold(front))
+                {
+                    i++;
+                    _position = front;
+                }
+                else
+                {
+                    if (i > 0)
+                    {
+                        yield return i.ToString();
+                        i = 0;
+                    }
+
+                    var left = _position.Move(_direction.TurnLeft());
+                    if (_grid.IsScaffold(left))
+                    {
+                        _direction = _direction.TurnLeft();
+                        yield return "L";
+                    }
+                    else
+                    {
+                        var right = _position.Move(_direction.TurnRight());
+                        if (_grid.IsScaffold(right))
+                        {
+                            _direction = _direction.TurnRight();
+                            yield return "R";
+                        }
+                        else
+                        {
+                            finished = true;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
