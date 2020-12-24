c24e1e9da5df24fb2c5e544a7d53f61f5027ebc9
Piotr Szulc <szulo123@gmail.com>
2020-12-24 10:21:24 +0100

    Solved 2020/21

---
+++ 2020/AOC2020/Day21/Puzzle.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AOC2020.Day21
+{
+    public class Puzzle : PuzzleBase<int, string>
+    {
+        public Puzzle(IEnumerable<string> input)
+        {
+            _input = input.Select(ParseFood).ToArray();
+        }
+
+        private readonly Food[] _input;
+
+        private record Food(string[] Ingredients, string[] Allergens);
+
+        private static readonly Regex FoodRegex = new Regex(@"^(?<ingredients>.+)\s\(contains (?<allergens>.+)\)$");
+        private static Food ParseFood(string line)
+        {
+            if (!FoodRegex.TryMatch(line, out var match))
+            {
+                throw new PuzzleException("Invalid line format");
+            }
+
+            var ingredients = match.Groups["ingredients"].Value.Split(" ", System.StringSplitOptions.TrimEntries);
+            var allergens = match.Groups["allergens"].Value.Split(", ", System.StringSplitOptions.TrimEntries);
+
+            return new Food(ingredients, allergens);
+        }
+
+        private static Dictionary<string, Food[]> GetFoodByAllergen(IEnumerable<Food> food) =>
+            food.SelectMany(f => f.Allergens.Select(a => (a, f)))
+                .GroupBy(p => p.a)
+                .ToDictionary(g => g.Key, g => g.Select(p => p.f).ToArray());
+
+        private static IEnumerable<string> GetIntersecting(IEnumerable<IEnumerable<string>> ingredients) =>
+            ingredients.Aggregate((a, b) => a.Intersect(b));
+
+        public override int Solution1()
+        {
+            var knownIngredients = MatchIngredientsToAllergens();
+            return _input.SelectMany(f => f.Ingredients)
+                .Count(i => !knownIngredients.ContainsKey(i));
+        }
+
+        private Dictionary<string, string> MatchIngredientsToAllergens()
+        {
+            var knownIngredients = new Dictionary<string, string>();
+
+            var dict = GetFoodByAllergen(_input);
+            var unmatchedAllergens = new Queue<string>(dict.Keys);
+
+            while (unmatchedAllergens.Any())
+            {
+                var allergen = unmatchedAllergens.Dequeue();
+                var commonIngredients = GetIntersecting(
+                    dict[allergen].Select(f => f.Ingredients.Where(i => !knownIngredients.ContainsKey(i))))
+                    .ToArray();
+
+                if (commonIngredients.Length == 1)
+                {
+                    knownIngredients.Add(commonIngredients.First(), allergen);
+                }
+                else
+                {
+                    unmatchedAllergens.Enqueue(allergen);
+                }
+            }
+
+            return knownIngredients;
+        }
+
+        public override string Solution2()
+        {
+            var ingredients = MatchIngredientsToAllergens()
+                .Select(kv => (kv.Key, kv.Value))
+                .OrderBy(p => p.Value)
+                .Select(p => p.Key);
+
+            return string.Join(",", ingredients);
+        }
+    }
+}
---
+++ 2020/Tests/Day21Tests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using AOC2020.Day21;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Day21Tests
+    {
+        private static readonly string[] ExampleData =
+        {
+            "mxmxvkd kfcds sqjhc nhms (contains dairy, fish)",
+            "trh fvjkl sbzzf mxmxvkd (contains dairy)",
+            "sqjhc fvjkl (contains soy)",
+            "sqjhc mxmxvkd sbzzf (contains fish)",
+        };
+
+        private readonly Puzzle _example = new Puzzle(ExampleData);
+
+        [Test]
+        public void Solution1Test()
+        {
+            _example.Solution1().Should().Be(5);
+        }
+
+        [Test]
+        public void Solution2Test()
+        {
+            _example.Solution2().Should().Be("mxmxvkd,sqjhc,fvjkl");
+        }
+    }
+}
