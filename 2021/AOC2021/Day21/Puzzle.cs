b7f0bb8e5b1d94ba7766e2c98fc085087341e1ed
Piotr Szulc <szulo123@gmail.com>
2021-12-22 12:52:24 +0100

    Solved 2021/21 part 1

---
+++ 2021/AOC2021/Day21/Puzzle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Text.RegularExpressions;
+using AOC2021.Common;
+
+namespace AOC2021.Day21
+{
+    public class Puzzle : PuzzleBase<long, long>
+    {
+        public Puzzle(IEnumerable<string> lines)
+        {
+            _player1 = _inputPattern.Parse(lines.ElementAt(0), "pos", int.Parse);
+            _player2 = _inputPattern.Parse(lines.ElementAt(1), "pos", int.Parse);
+        }
+
+        private static readonly Regex _inputPattern = new(@"^Player \d+ starting position: (?<pos>\d+)$", RegexOptions.Compiled);
+
+        private readonly int _player1;
+        private readonly int _player2;
+
+        public override long Solution1()
+        {
+            var positions = new[] { _player1 - 1, _player2 - 1 };
+            var score = new[] { 0, 0 };
+            var player = 0;
+
+            var die = new DeterministicDie();
+
+            while (score[0] < 1000 && score[1] < 1000)
+            {
+                positions[player] = (positions[player] + die.Take(3).Sum()).Modulo(10);
+                score[player] += positions[player] + 1;
+                player = (player + 1) % 2;
+            }
+
+            return score[player] * die.RolledCount;
+        }
+
+        public override long Solution2()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class DeterministicDie
+        {
+            public int RolledCount { get; private set; } = 0;
+            private int _current = 1;
+
+            public int Next()
+            {
+                RolledCount++;
+                var c = _current;
+                _current++;
+                if (_current > 100)
+                {
+                    _current = 1;
+                }
+                return c;
+            }
+
+            public IEnumerable<int> Take(int count) =>
+                Enumerable.Range(0, count).Select(i => Next());
+        }
+    }
+}
---
+++ 2021/AocTests/Day21Tests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using AOC2021.Day21;
+using FluentAssertions;
+
+namespace AocTests
+{
+    public class Day21Tests
+    {
+        private static readonly string[] _testInput = new string[]
+        {
+            "Player 1 starting position: 4",
+            "Player 2 starting position: 8",
+        };
+
+        private readonly Puzzle _puzzle = new(_testInput);
+
+        [Test]
+        public void Solution1Test()
+        {
+            _puzzle.Solution1().Should().Be(739785);
+        }
+
+        [Test]
+        public void Solution2Test()
+        {
+            _puzzle.Solution2().Should().Be(0);
+        }
+    }
+}
