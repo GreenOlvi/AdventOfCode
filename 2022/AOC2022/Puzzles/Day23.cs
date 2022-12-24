8ecccdd5fc8b7d333e8d4d2615371cdf216d27b7
Piotr Szulc <szulo123@gmail.com>
2022-12-24 14:11:58 +0100

    WIP

---
+++ 2022/AOC2022/Puzzles/Day23.cs
@@ -0,0 +1,50 @@
+﻿namespace AOC2022.Puzzles;
+
+public class Day23 : CustomBaseDay
+{
+    private readonly HashGrid<bool> _initialGrid;
+
+    public Day23()
+    {
+        _initialGrid = ParseInput(ReadLinesFromFile());
+    }
+
+    public Day23(IEnumerable<string> lines)
+    {
+        _initialGrid = ParseInput(lines);
+    }
+
+    private static HashGrid<bool> ParseInput(IEnumerable<string> lines)
+    {
+        var grid = new HashGrid<bool>(new Dictionary<bool, char>()
+        {
+            [true] = '#',
+            [false] = '.',
+        });
+
+        var y = 0;
+        foreach (var line in lines)
+        {
+            for (var x = 0; x < line.Length; x++)
+            {
+                if (line[x] == '#')
+                {
+                    grid[(x, y)] = true;
+                }
+            }
+            y++;
+        }
+        return grid;
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        var s = _initialGrid.Draw();
+        return "result1".ToResult();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        return "result2".ToResult();
+    }
+}
---
+++ 2022/Tests/Day23Tests.cs
@@ -0,0 +1,30 @@
+namespace Tests;
+
+[TestFixture]
+public class Day23Tests
+{
+    private readonly IEnumerable<string> _testInput = new string[]
+    {
+        "....#..",
+        "..###.#",
+        "#...#.#",
+        ".#...##",
+        "#.###..",
+        "##.#.##",
+        ".#..#..",
+    };
+
+    [Test]
+    public async ValueTask Solve1Test()
+    {
+        var puzzle = new Day23(_testInput);
+        (await puzzle.Solve_1()).Should().Be("result1");
+    }
+
+    [Test]
+    public async ValueTask Solve2Test()
+    {
+        var puzzle = new Day23(_testInput);
+        (await puzzle.Solve_2()).Should().Be("result2");
+    }
+}
