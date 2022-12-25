fc7582a736d4893a171f3059eb8edd874bd71f00
Piotr Szulc <szulo123@gmail.com>
2022-12-25 10:15:17 +0100

    Extracted Box class to Common

---
+++ 2022/AOC2022/Common/Box.cs
@@ -0,0 +1,13 @@
+﻿namespace AOC2022.Common;
+
+public readonly record struct Box(Point2 TopLeft, Point2 BottomRight)
+{
+    public long Width => BottomRight.X - TopLeft.X + 1;
+    public long Height => BottomRight.Y - TopLeft.Y + 1;
+
+    public bool IsInside(Point2 point) =>
+        TopLeft.X <= point.X &&
+        TopLeft.Y <= point.Y &&
+        BottomRight.X >= point.X &&
+        BottomRight.Y >= point.Y;
+}
--- 2022/AOC2022/Puzzles/Day24.cs
+++ 2022/AOC2022/Puzzles/Day24.cs
@@ -141,16 +141,4 @@ public class Day24 : CustomBaseDay
             return this with { Position = wrapped };
         }
     }
-
-    private readonly record struct Box(Point2 TopLeft, Point2 BottomRight)
-    {
-        public long Width => BottomRight.X - TopLeft.X + 1;
-        public long Height => BottomRight.Y - TopLeft.Y + 1;
-
-        public bool IsInside(Point2 point) =>
-            TopLeft.X <= point.X &&
-            TopLeft.Y <= point.Y &&
-            BottomRight.X >= point.X &&
-            BottomRight.Y >= point.Y;
-    }
 }
