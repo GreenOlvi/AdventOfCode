705c9cb0abb730647aee7528cdedf4495ea00ed5
Piotr Szulc <szulo123@gmail.com>
2023-12-04 07:03:47 +0100

    Solved 2023/04

---
+++ 2023/AOC2023/Puzzles/Day04.cs
@@ -0,0 +1,67 @@
+﻿using AOC2022.Common;
+
+namespace AOC2023.Puzzles;
+public partial class Day04 : CustomBaseDay
+{
+    private readonly Card[] _cards;
+
+    public Day04()
+    {
+        _cards = ReadLinesFromFile().Select(ParseLine).OrderBy(c => c.Id).ToArray();
+    }
+
+    public Day04(IEnumerable<string> lines)
+    {
+        _cards = lines.Select(ParseLine).OrderBy(c => c.Id).ToArray();
+    }
+
+    private static readonly Regex CardMatch = BuildCardMatch();
+    private static Card ParseLine(string line)
+    {
+        if (!CardMatch.TryMatch(line, out var match))
+        {
+            throw new InvalidDataException(line);
+        }
+
+        var id = int.Parse(match.Groups["id"].Value);
+        var winning = match.Groups["winning"].Value
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(int.Parse)
+            .ToHashSet();
+
+        var my = match.Groups["my"].Value
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+            .Select(int.Parse);
+
+        var won = my.Count(winning.Contains);
+
+        return new Card(id, won);
+    }
+
+    public override ValueTask<string> Solve_1() =>
+        _cards.Sum(c => c.WinCount > 0 ? 1L << (c.WinCount - 1) : 0).ToResult();
+
+    public override ValueTask<string> Solve_2()
+    {
+        var cardCount = _cards.Select(c => (c.Id, 1L)).ToDictionary();
+        var maxId = _cards.Last().Id;
+        var sum = 0L;
+
+        foreach (var card in _cards)
+        {
+            var count = cardCount[card.Id];
+            sum += count;
+            for (var i = card.Id + 1; i <= maxId && i <= card.Id + card.WinCount; i++)
+            {
+                cardCount[i] += count;
+            }
+        }
+
+        return sum.ToResult();
+    }
+
+    private readonly record struct Card(int Id, int WinCount);
+
+    [GeneratedRegex(@"^Card\s+(?<id>\d+):\s+(?<winning>.+)\s+\|\s+(?<my>.+)$")]
+    private static partial Regex BuildCardMatch();
+}
---
+++ 2023/Tests/Puzzles/Day04Test.cs
@@ -0,0 +1,28 @@
+namespace Tests.Puzzles;
+
+public class Day04Tests
+{
+    private static readonly string _testInput1 = """
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+""";
+
+
+    [Test]
+    public async ValueTask Solve1Test()
+    {
+        var day = new Day04(_testInput1.Split("\n", StringSplitOptions.TrimEntries));
+        (await day.Solve_1()).Should().Be("13");
+    }
+
+    [Test]
+    public async ValueTask Solve2Test()
+    {
+        var day = new Day04(_testInput1.Split("\n", StringSplitOptions.TrimEntries));
+        (await day.Solve_2()).Should().Be("30");
+    }
+}
