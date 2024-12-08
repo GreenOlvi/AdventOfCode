eeeede15cf7bc682c182dd5fe9d6a7b3bca693d6
Piotr Szulc <szulo123@gmail.com>
2024-12-08 14:31:01 +0100

    Extracted IHashGrid interface

--- 2024/AOC2024/Common/HashGrid.cs
+++ 2024/AOC2024/Common/HashGrid.cs
@@ -1,33 +1,62 @@
 ﻿using System.Collections;
+using System.Collections.Frozen;
 
 namespace AOC2024.Common;
 
-public sealed class HashGrid<TTile>(TTile defaultValue = default) : IEnumerable<(Point2 Position, TTile Tile)> where TTile : struct
+public interface IHashGrid<TTile> : IEnumerable<(Point2 Position, TTile Tile)> where TTile : struct
+{
+    TTile this[(long x, long y) p] { get; set; }
+    TTile this[(int x, int y) p] { get; set; }
+    TTile this[Point2 p] { get; set; }
+
+    long MaxY { get; }
+    long MinY { get; }
+    long MinX { get; }
+    long MaxX { get; }
+    Point2 TopLeft { get; }
+    Point2 BottomRight { get; }
+    Box Box { get; }
+    TTile DefaultTile { get; }
+
+    IEnumerable<Point2> FindTiles(Func<TTile, bool> predicate);
+    Box GetSurroundingBox();
+}
+
+public sealed class HashGrid<TTile>(TTile defaultValue = default) : IHashGrid<TTile> where TTile : struct
 {
     private readonly Dictionary<Point2, TTile> _tiles = [];
 
     private HashGrid(IEnumerable<KeyValuePair<Point2, TTile>> tiles, TTile defaultValue = default)
         : this(defaultValue)
     {
-        _tiles = tiles.ToDictionary();
+        _tiles = tiles.Where(t => !t.Value.Equals(defaultValue))
+            .ToDictionary();
     }
 
     public TTile this[(long x, long y) p]
     {
-        get => _tiles.TryGetValue(new Point2(p), out var tile) ? tile : Default;
-        set => _tiles[new Point2(p)] = value;
+        get => _tiles.TryGetValue(new Point2(p), out var tile) ? tile : DefaultTile;
+        set => SetValue(new Point2(p), value);
     }
 
     public TTile this[(int x, int y) p]
     {
-        get => _tiles.TryGetValue(new Point2(p), out var tile) ? tile : Default;
-        set => _tiles[new Point2(p)] = value;
+        get => _tiles.TryGetValue(new Point2(p), out var tile) ? tile : DefaultTile;
+        set => SetValue(new Point2(p), value);
     }
 
     public TTile this[Point2 p]
     {
-        get => _tiles.TryGetValue(p, out var tile) ? tile : Default;
-        set => _tiles[p] = value;
+        get => _tiles.TryGetValue(p, out var tile) ? tile : DefaultTile;
+        set => SetValue(p, value);
+    }
+
+    private void SetValue(Point2 p, TTile v)
+    {
+        if (!v.Equals(DefaultTile))
+        {
+            _tiles[p] = v;
+        }
     }
 
     public long MaxY => _tiles.Keys.Max(static p => p.Y);
@@ -40,7 +69,7 @@ public sealed class HashGrid<TTile>(TTile defaultValue = default) : IEnumerable<
 
     public Box Box => new(TopLeft, BottomRight);
 
-    public TTile Default { get; } = defaultValue;
+    public TTile DefaultTile { get; } = defaultValue;
 
     public IEnumerable<(Point2 Position, TTile Tile)> Where(Func<(Point2 Position, TTile Tile), bool> predicate) =>
         _tiles.Select(static kv => (Position: kv.Key, Tile: kv.Value))
@@ -52,10 +81,78 @@ public sealed class HashGrid<TTile>(TTile defaultValue = default) : IEnumerable<
 
     public Box GetSurroundingBox() => new(new Point2(MinX, MinY), new Point2(MaxX, MaxY));
 
-    public HashGrid<TTile> Clone() => new(_tiles, Default);
+    public HashGrid<TTile> Clone() => new(_tiles, DefaultTile);
+    public FrozenHashGrid<TTile> ToFrozen() => new(_tiles, DefaultTile);
 
     public IEnumerator<(Point2 Position, TTile Tile)> GetEnumerator() =>
-        _tiles.Where(t => !t.Value.Equals(Default))
+        _tiles.Where(t => !t.Value.Equals(DefaultTile))
+            .Select(kv => (kv.Key, kv.Value))
+            .GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
+public sealed class FrozenHashGrid<TTile> : IHashGrid<TTile> where TTile : struct
+{
+    private readonly FrozenDictionary<Point2, TTile> _tiles;
+
+    public FrozenHashGrid(IEnumerable<KeyValuePair<Point2, TTile>> tiles, TTile defaultValue = default)
+    {
+        _tiles = tiles.Where(t => !t.Value.Equals(defaultValue))
+            .ToFrozenDictionary();
+
+        DefaultTile = defaultValue;
+        MaxY = _tiles.Keys.Max(static p => p.Y);
+        MinY = _tiles.Keys.Min(static p => p.Y);
+        MaxX = _tiles.Keys.Min(static p => p.X);
+        MinX = _tiles.Keys.Max(static p => p.X);
+        TopLeft = new(MinX, MinY);
+        BottomRight = new(MaxX, MaxY);
+        Box = new(TopLeft, BottomRight);
+    }
+
+    public TTile this[(long x, long y) p]
+    {
+        get => _tiles.TryGetValue(new Point2(p), out var tile) ? tile : DefaultTile;
+        set => throw new InvalidOperationException();
+    }
+
+    public TTile this[(int x, int y) p]
+    {
+        get => _tiles.TryGetValue(new Point2(p), out var tile) ? tile : DefaultTile;
+        set => throw new InvalidOperationException();
+    }
+
+    public TTile this[Point2 p]
+    {
+        get => _tiles.TryGetValue(p, out var tile) ? tile : DefaultTile;
+        set => throw new InvalidOperationException();
+    }
+
+    public long MaxY { get; }
+    public long MinY { get; }
+    public long MinX { get; }
+    public long MaxX { get; }
+
+    public Point2 TopLeft { get; }
+    public Point2 BottomRight { get; }
+
+    public Box Box { get; }
+
+    public TTile DefaultTile { get; }
+
+    public IEnumerable<(Point2 Position, TTile Tile)> Where(Func<(Point2 Position, TTile Tile), bool> predicate) =>
+        _tiles.Select(static kv => (Position: kv.Key, Tile: kv.Value))
+            .Where(predicate);
+
+    public IEnumerable<Point2> FindTiles(Func<TTile, bool> predicate) =>
+        Where(p => predicate(p.Tile))
+            .Select(p => p.Position);
+
+    public Box GetSurroundingBox() => new(new Point2(MinX, MinY), new Point2(MaxX, MaxY));
+
+
+    public IEnumerator<(Point2 Position, TTile Tile)> GetEnumerator() =>
+        _tiles.Where(t => !t.Value.Equals(DefaultTile))
             .Select(kv => (kv.Key, kv.Value))
             .GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
@@ -63,7 +160,7 @@ public sealed class HashGrid<TTile>(TTile defaultValue = default) : IEnumerable<
 
 public static class HashGridExtensions
 {
-    public static string Draw<T>(this HashGrid<T> grid, Func<Point2, T, char> tileToChar, Point2 topLeft, Point2 bottomRight) where T : struct
+    public static string Draw<T>(this IHashGrid<T> grid, Func<Point2, T, char> tileToChar, Point2 topLeft, Point2 bottomRight) where T : struct
     {
         var sb = new StringBuilder();
         for (var y = topLeft.Y; y <= bottomRight.Y; y++)
@@ -78,7 +175,7 @@ public static class HashGridExtensions
         return sb.ToString();
     }
 
-    public static string Draw<T>(this HashGrid<T> grid, Func<Point2, T, string> tileToStr, Point2 topLeft, Point2 bottomRight) where T : struct
+    public static string Draw<T>(this IHashGrid<T> grid, Func<Point2, T, string> tileToStr, Point2 topLeft, Point2 bottomRight) where T : struct
     {
         var sb = new StringBuilder();
         for (var y = topLeft.Y; y <= bottomRight.Y; y++)
@@ -93,7 +190,7 @@ public static class HashGridExtensions
         return sb.ToString();
     }
 
-    public static void Print<T>(this HashGrid<T> grid, Func<Point2, T, (ConsoleColor?, string)> tileToStr, Point2 topLeft, Point2 bottomRight) where T : struct
+    public static void Print<T>(this IHashGrid<T> grid, Func<Point2, T, (ConsoleColor?, string)> tileToStr, Point2 topLeft, Point2 bottomRight) where T : struct
     {
         var defaultColor = Console.ForegroundColor;
 
@@ -114,19 +211,19 @@ public static class HashGridExtensions
     }
 
 
-    public static string Draw<T>(this HashGrid<T> grid, Func<T, char> tileToChar, Point2 topLeft, Point2 bottomRight) where T : struct =>
+    public static string Draw<T>(this IHashGrid<T> grid, Func<T, char> tileToChar, Point2 topLeft, Point2 bottomRight) where T : struct =>
         Draw(grid, (_, t) => tileToChar(t), topLeft, bottomRight);
 
-    public static string Draw<T>(this HashGrid<T> grid, Func<T, string> tileToStr, Point2 topLeft, Point2 bottomRight) where T : struct =>
+    public static string Draw<T>(this IHashGrid<T> grid, Func<T, string> tileToStr, Point2 topLeft, Point2 bottomRight) where T : struct =>
         Draw(grid, (_, t) => tileToStr(t), topLeft, bottomRight);
 
-    public static string Draw<T>(this HashGrid<T> grid, Func<T, char> tileToChar) where T : struct =>
+    public static string Draw<T>(this IHashGrid<T> grid, Func<T, char> tileToChar) where T : struct =>
         grid.Draw(tileToChar, grid.TopLeft - Point2.One, grid.BottomRight + Point2.One);
 
-    public static string Draw<T>(this HashGrid<T> grid, IDictionary<T, char> tileToChar, Point2 topLeft, Point2 bottomRight) where T : struct =>
+    public static string Draw<T>(this IHashGrid<T> grid, IDictionary<T, char> tileToChar, Point2 topLeft, Point2 bottomRight) where T : struct =>
         Draw(grid, t => tileToChar[t], topLeft, bottomRight);
 
-    public static string Draw<T>(this HashGrid<T> grid, IDictionary<T, char> tileChars) where T : struct =>
+    public static string Draw<T>(this IHashGrid<T> grid, IDictionary<T, char> tileChars) where T : struct =>
         Draw(grid, t => tileChars[t]);
 
     private static readonly Dictionary<bool, char> _boolTileChars = new()
@@ -135,9 +232,9 @@ public static class HashGridExtensions
         [true] = '#',
     };
 
-    public static string Draw(this HashGrid<bool> grid) => Draw(grid, _boolTileChars);
+    public static string Draw(this IHashGrid<bool> grid) => Draw(grid, _boolTileChars);
 
-    public static string Draw(this HashGrid<char> grid) => Draw(grid, static c => c == '\0' ? ' ' : c);
-    public static string Draw(this HashGrid<char> grid, Point2 topLeft, Point2 bottomRight) =>
+    public static string Draw(this IHashGrid<char> grid) => Draw(grid, static c => c == '\0' ? ' ' : c);
+    public static string Draw(this IHashGrid<char> grid, Point2 topLeft, Point2 bottomRight) =>
         Draw(grid, static c => c == '\0' ? ' ' : c, topLeft, bottomRight);
 }
