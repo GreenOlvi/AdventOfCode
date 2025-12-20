6659273930d8bcc5ec683a342b693a5fbfbfaedd
Piotr Szulc <szulo123@gmail.com>
2025-12-20 12:16:23 +0100

    Solved 2025/09 part 2

--- 2025/AOC2025/Puzzles/Day09.cs
+++ 2025/AOC2025/Puzzles/Day09.cs
@@ -1,4 +1,3 @@
-
 namespace AOC2025.Puzzles;
 
 public class Day09 : CustomBaseProblem<long>
@@ -32,6 +31,48 @@ public class Day09 : CustomBaseProblem<long>
 
     public override long Solve2()
     {
-        return default;
+        var lines = _input.Pairwise()
+            .Select(pair => new Line(pair.Item1, pair.Item2))
+            .Append(new Line(_input[^1], _input[0]))
+            .ToArray();
+
+        var horizontal = lines.Where(l => l.A.Y == l.B.Y)
+            .Select(l => l.A.X < l.B.X ? l : new Line(l.B, l.A))
+            .OrderBy(l => l.A.Y)
+            .ToArray();
+        var vertical = lines.Where(l => l.A.X == l.B.X)
+            .Select(l => l.A.Y < l.B.Y ? l : new Line(l.B, l.A))
+            .OrderBy(l => l.A.X)
+            .ToArray();
+
+        bool IsOk((Point2 A, Point2 B) pair)
+        {
+            var box = Box.FromTwoPoints(pair.A, pair.B);
+            var hor = horizontal.Where(l => l.A.Y > box.TopLeft.Y
+                    && l.A.Y < box.BottomRight.Y
+                    && box.TopLeft.X < l.B.X
+                    && box.BottomRight.X > l.A.X);
+
+            if (hor.Any())
+            {
+                return false;
+            }
+
+            var ver = vertical.Where(l => l.A.X > box.TopLeft.X
+                    && l.A.X < box.BottomRight.X
+                    && box.TopLeft.Y < l.B.Y
+                    && box.BottomRight.Y > l.A.Y);
+
+            return !ver.Any();
+        }
+
+        var max = _input.EachPair()
+            .Select(p => (Points: p, Area: Area(p)))
+            .OrderByDescending(p => p.Area)
+            .First(p => IsOk(p.Points));
+
+        return max.Area;
     }
+
+    private readonly record struct Line(Point2 A, Point2 B);
 }
